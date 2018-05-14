5641f836aed939cafac74571cde50df4898cf654
L <l@tengio.com>
2018-05-14 15:57:20 +0100

    hacking new converter

--- src/transformers.js
+++ src/transformers.js
@@ -1,12 +1,15 @@
 
 export const DEFAULT_CONVERTER = 'rgba_hex'
 export const converters = {
-  rgba: c => `rgba(${c.rgb.r}, ${c.rgb.g}, ${c.rgb.b}, ${c.rgb.a})`,
-  rgb: c => `rgb(${c.rgb.r}, ${c.rgb.g}, ${c.rgb.b})`,
-  hex: c => c.hex,
-
-  rgba_rgb: c => c.rgb.a === 1 ? converters.rgb(c) : converters.rgba(c),
-  rgba_hex: c => c.rgb.a === 1 ? converters.hex(c) : converters.rgba(c)
+  rgba_hex: c => convert(c)
 }
+
+const convert = c => {
+    const a = (255*c.a < 16 ? "0" : "") + Math.round(255*c.a).toString(16)
+    const r = (255*c.r < 16 ? "0" : "") + Math.round(255*c.r).toString(16)
+    const g = (255*c.g < 16 ? "0" : "") + Math.round(255*c.g).toString(16)
+    const b = (255*c.b < 16 ? "0" : "") + Math.round(255*c.b).toString(16)
+    return "#" + a + r + g + b
+} 
 
 export default converters
